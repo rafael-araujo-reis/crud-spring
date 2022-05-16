eca19193c4375b7d845be7581b0e7aff901d9d50
Rafael Reis <rafa.araujoreis@gmail.com>
2022-05-16 13:27:19 -0300

    change class to health

--- src/main/java/dev/rafaelreis/crudspring/controller/HealthController.java
+++ src/main/java/dev/rafaelreis/crudspring/controller/HealthController.java
@@ -2,16 +2,14 @@ package dev.rafaelreis.crudspring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/hello")
-public class HelloController {
+@RequestMapping("/health")
+public class HealthController {
 
   @GetMapping
-  public String hello() {
-    return "Hello word!";
+  public String health() {
+    return "UP";
   }
 }
