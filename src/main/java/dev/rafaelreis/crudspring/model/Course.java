d833599e32648498f156f3a2025e538da196256a
Rafael Reis <rafa.araujoreis@gmail.com>
2022-05-16 18:44:14 -0300

    add anotation JsonProperties

--- src/main/java/dev/rafaelreis/crudspring/model/Course.java
+++ src/main/java/dev/rafaelreis/crudspring/model/Course.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 // import javax.persistence.Table;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
@@ -16,6 +18,7 @@ public class Course {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @JsonProperty("_id")
   private Long id;
 
   // @Column(name = "coluna_nome_diferente")
