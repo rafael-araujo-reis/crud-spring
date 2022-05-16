d0d81feb5a1148a7a4689c4140381e7823e5bf7c
Rafael Reis <rafa.araujoreis@gmail.com>
2022-05-16 13:27:37 -0300

    create model course

---
+++ src/main/java/dev/rafaelreis/crudspring/model/Course.java
@@ -0,0 +1,28 @@
+package dev.rafaelreis.crudspring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+// import javax.persistence.Table;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+// @Table(name = "tabela_nome_diferente")
+public class Course {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  // @Column(name = "coluna_nome_diferente")
+  @Column(length = 200, nullable = false)
+  private String name;
+
+  @Column(length = 20, nullable = false)
+  private String category;
+
+}
