ef876898a44ea79ff84cd2e9a7d32655e9b4a0db
Rafael Reis <rafa.araujoreis@gmail.com>
2022-05-19 18:58:16 -0300

    create method post

--- src/main/java/dev/rafaelreis/crudspring/controller/CoursesControler.java
+++ src/main/java/dev/rafaelreis/crudspring/controller/CoursesControler.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.rafaelreis.crudspring.model.Course;
@@ -26,6 +31,14 @@ public class CoursesControler {
   @GetMapping
   public List<Course> list() {
     return courseRepository.findAll();
+  }
+
+  @PostMapping
+  // @ResponseStatus(code = HttpStatus.CREATED)
+  public ResponseEntity<Course> create(@RequestBody Course course) {
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(courseRepository.save(course));
   }
 
   @Bean
